774a30f4ccaf916b096f0b1198273a5a25259184
ktdhhc <1642012059@qq.com>
2025-07-10 18:40:29 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import streamlit as st
-from langchain.memory import ConversationBufferMemory
+from utils import generate_script
 
-from utils import get_chat_response
-
-st.title("💬 克隆ChatGPT")
+st.title("🎬 视频脚本生成器")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
-    st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
+    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
+    st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
 
-if "memory" not in st.session_state:
-    st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
-    st.session_state["messages"] = [{"role": "ai",
-                                     "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
+subject = st.text_input("💡 请输入视频的主题")
+video_length = st.number_input("⏱️ 请输入视频的大致时长（单位：分钟）", min_value=0.1, step=0.1)
+creativity = st.slider("✨ 请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0,
+                       max_value=1.0, value=0.2, step=0.1)
+submit = st.button("生成脚本")
 
-for message in st.session_state["messages"]:
-    st.chat_message(message["role"]).write(message["content"])
-
-prompt = st.chat_input()
-if prompt:
-    if not openai_api_key:
-        st.info("请输入你的OpenAI API Key")
-        st.stop()
-    st.session_state["messages"].append({"role": "human", "content": prompt})
-    st.chat_message("human").write(prompt)
-
+if submit and not openai_api_key:
+    st.info("请输入你的OpenAI API密钥")
+    st.stop()
+if submit and not subject:
+    st.info("请输入视频的主题")
+    st.stop()
+if submit and not video_length >= 0.1:
+    st.info("视频长度需要大于或等于0.1")
+    st.stop()
+if submit:
     with st.spinner("AI正在思考中，请稍等..."):
-        response = get_chat_response(prompt, st.session_state["memory"],
-                                     openai_api_key)
-    msg = {"role": "ai", "content": response}
-    st.session_state["messages"].append(msg)
-    st.chat_message("ai").write(response)
+        search_result, title, script = generate_script(subject, video_length, creativity, openai_api_key)
+    st.success("视频脚本已生成！")
+    st.subheader("🔥 标题：")
+    st.write(title)
+    st.subheader("📝 视频脚本：")
+    st.write(script)
+    with st.expander("维基百科搜索结果 👀"):
+        st.info(search_result)
--- utils.py
+++ utils.py
@@ -1,18 +1,40 @@
-from langchain.chains import ConversationChain
+from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_community.utilities import WikipediaAPIWrapper
 
-import os
-from langchain.memory import ConversationBufferMemory
+# import os
 
+def generate_script(subject, video_length, creativity, api_key):
+    title_template = ChatPromptTemplate.from_messages(
+        [
+            ("human", "请为'{subject}'这个主题的视频想一个吸引人的标题")
+        ]
+    )
+    script_template = ChatPromptTemplate.from_messages(
+        [
+            ("human",
+             """你是一位短视频频道的博主。根据以下标题和相关信息，为短视频频道写一个视频脚本。
+             视频标题：{title}，视频时长：{duration}分钟，生成的脚本的长度尽量遵循视频时长的要求。
+             要求开头抓住限球，中间提供干货内容，结尾有惊喜，脚本格式也请按照【开头、中间，结尾】分隔。
+             整体内容的表达方式要尽量轻松有趣，吸引年轻人。
+             脚本内容可以结合以下维基百科搜索出的信息，但仅作为参考，只结合相关的即可，对不相关的进行忽略：
+             ```{wikipedia_search}```""")
+        ]
+    )
 
-def get_chat_response(prompt, memory, openai_api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
-    chain = ConversationChain(llm=model, memory=memory)
+    model = ChatOpenAI(openai_api_key=api_key, temperature=creativity)
 
-    response = chain.invoke({"input": prompt})
-    return response["response"]
+    title_chain = title_template | model
+    script_chain = script_template | model
 
+    title = title_chain.invoke({"subject": subject}).content
 
-# memory = ConversationBufferMemory(return_messages=True)
-# print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
-# print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
+    search = WikipediaAPIWrapper(lang="zh")
+    search_result = search.run(subject)
+
+    script = script_chain.invoke({"title": title, "duration": video_length,
+                                  "wikipedia_search": search_result}).content
+
+    return search_result, title, script
+
+# print(generate_script("sora模型", 1, 0.7, os.getenv("OPENAI_API_KEY")))
